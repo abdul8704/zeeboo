f12e8e2c9cddf3dc1757fd2f3a24653b40fb7d3f
abdul8704 <abdulaziz87049@gmail.com>
2025-05-21 23:00:43 +0530

    final

--- src/components/TypingNote.jsx
+++ src/components/TypingNote.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 
 const TYPING_SPEED = 100; // milliseconds per character
-
+const message = "YYou're reallyyy one of the bestestest person i have ever shared my days with(i really mean it). you're the only person i was able to open up and rant out(i felt alive)... i truly really sincerely, believe that you deserve aaaaallll the love in the world...\n i hope this year has your eyes sparkling all the time... \nWishing you a veryyyyyyy Happy Birthday!!"
 const centerStyle = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     height: "100vh",
-    fontSize: "2.5rem",
+    fontSize: "1.9rem",
     fontFamily: "'Poppins', monospace",
     color: "#fff",
     fontStyle: "italic",
     background: "transparent",
 };
 
-const TypingNote = ({ text = "aabbcded Many more Happy Birthday!" }) => {
+const TypingNote = ({ text = message }) => {
     const [displayed, setDisplayed] = useState("");
 
     useEffect(() => {
         setDisplayed("");
         let i = 0;
         const interval = setInterval(() => {
-            if (i < text.length-1) {
+            if (i < text.length - 1) {
                 setDisplayed((prev) => prev + text[i]);
                 i++;
             } else {
@@ -34,7 +34,9 @@ const TypingNote = ({ text = "aabbcded Many more Happy Birthday!" }) => {
     return (
         <div style={centerStyle}>
             {displayed}
-            <span style={{ opacity: 0.5 }}>{displayed.length < text.length ? "|" : ""}</span>
+            <span style={{ opacity: 0.5 }}>
+                {displayed.length < text.length ? "|" : ""}
+            </span>
         </div>
     );
 };
